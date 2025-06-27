aa4c4d0915389da708068983d6fdef6870b4e319
Sooraj E P <ep@epLapMac.local>
2025-06-27 15:32:03 +0530

    Fix-trace-connect

--- setup.sh
+++ setup.sh
@@ -70,7 +70,7 @@ PARENT_DIR=$(dirname "$SCRIPT_DIR")
 PROJECT_DIR="$PARENT_DIR/trace_connect"
 
 # Clone the repository
-REPO_URL="https://github.com/Fairfood/Trace-Server.git"
+REPO_URL="https://github.com/Fairfood/Connect-Server.git"
 BRANCH="main"
 if [ ! -d "$PROJECT_DIR" ]; then
     echo "Cloning the repository to $PROJECT_DIR..."
